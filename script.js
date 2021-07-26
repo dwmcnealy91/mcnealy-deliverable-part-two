4601688b2daf06bb65cd7be776db33850e910df3
derek <derek.mcnealy91@gmail.com>
2021-07-26 11:52:41 -0400

    First commit for deliverable two

---
+++ script.js
@@ -0,0 +1,25 @@
+var petName = prompt("Name your pet.");
+
+var answer = ""
+
+//user enters name and the next prompt starts the reactions
+
+answer = prompt ["feed, pet, or walk?"]; // at the end, after the six actions, log the pet's name, happiness, and energy level to the console. (console log)
+let energyTotal = 0;
+let happinessToal = 0;
+for (let i = 0; i < 2; i++) {
+    let feed = prompt ("feed, pet, or walk?");
+    let pet = prompt ("feed, pet, or walk?");
+    let walk = prompt ("feed, pet, or walk?");
+}
+
+
+
+//now need a function for feed, pet, and walk.
+
+if (total <= 0) {
+    alert("Not enough energy to enjoy a walk.")
+} else {
+
+}
+
